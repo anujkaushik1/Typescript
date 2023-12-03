0f1061f5494347be4e3400f74457ff793118a06d
Anuj Kaushik <anujkaushik1512@gmail.com>
2023-12-03 12:10:37 +0530

    functions

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+var main = function (a, b, c) {
+    if (b === void 0) { b = 10; }
+    console.log(a, "--------", b, "-------", c);
+};
+main("hahahaha", 6);
---
+++ functions/index.ts
@@ -0,0 +1,6 @@
+
+const main = (a : string, b : number = 10, c ? : number) : void => {
+    console.log(a , "--------", b, "-------", c);
+}
+
+main("hahahaha",6);
