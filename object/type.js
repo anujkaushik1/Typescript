d7f15b399fd412a2ecb83f3dd27f19571767eae6
Anuj Kaushik <anujkaushik1512@gmail.com>
2023-12-03 18:25:05 +0530

    passing obj in parameters

--- object/type.js
+++ object/type.js
@@ -10,3 +10,17 @@ var getDetails = function (details) {
     console.log(details.name);
 };
 getDetails(details);
+// Define the function that takes an object as an argument
+function processObject(obj) {
+    // Do something with the object
+    console.log(obj.name);
+    // You can access object properties or perform operations here
+}
+// Create an object
+var myObject = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+// Pass the object to the function
+processObject(myObject);
--- object/type.ts
+++ object/type.ts
@@ -14,8 +14,28 @@ const details : Details = {
     },
 }
 
+
+
 const getDetails = (details : Details) => {
     console.log(details.name)
 }
 
 getDetails(details);
+
+
+// // Define the function that takes an object as an argument
+// function processObject(obj: any) {
+//     // Do something with the object
+//     console.log(obj.name);
+//     // You can access object properties or perform operations here
+// }
+
+// // Create an object
+// const myObject = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
+
+// // Pass the object to the function
+// processObject(myObject);
